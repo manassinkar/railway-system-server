44567a8dcf3e32c04212074fa6fcc42b3478df5c
Manas Sinkar <manas.sinkar@gmail.com>
2020-03-08 11:58:55 +0530

    Test

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -262,6 +262,7 @@ function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
         ; 
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
     var d = R * c; // Distance in km
+    console.log("Distance",d);
     return d;
 }
   
