475e7e1e62f8cd3f8eadd56e3193d6a9302d59e9
Manas Sinkar <manas.sinkar@gmail.com>
2020-03-08 13:06:08 +0530

    Model Bug Fixed

--- models/user.model.js
+++ models/user.model.js
@@ -6,7 +6,7 @@ var occupied = new Schema({
     bookingID: { type: String, required: true },
     PNR: { type: String, required: true },
     trainNo: { type: Number, required: true },
-    coachName: { type: Number, required: true },
+    coachName: { type: String, required: true },
     seatNo: { type: Number, required: true },
     age: { type: Number, required: true }
 },{_id: false});
@@ -34,3 +34,47 @@ user.pre("save", function(next)
 
 var User = mongoose.model('user',user);
 module.exports = User;
+
+// "aadhaarNo":765974875989,
+// "firstName":"Harsh",
+// "lastName":"Shelar",
+// "irctcID":"harsh12345",
+// "email":"harshshelar22@gmail.com",
+// "contactNo":9821856556,
+// "password":"harsh12345"
+
+
+// "aadhaarNo":888888888888,
+// "firstName":"TC",
+// "lastName":"TC",
+// "irctcID":"tc12345",
+// "email":"tc@gmail.com",
+// "contactNo":9999999999,
+// "password":"tc12345"
+
+
+// "aadhaarNo":674611536346,
+// "firstName":"Manas",
+// "lastName":"Sinkar",
+// "irctcID":"manas12345",
+// "email":"manas.sinkar@gmail.com",
+// "contactNo":9022942188,
+// "password":"manas12345"
+
+
+// "aadhaarNo":634340477195,
+// "firstName":"Atul",
+// "lastName":"Kamble",
+// "irctcID":"atul12345",
+// "email":"kambleatul26@gmail.com",
+// "contactNo":8976490298,
+// "password":"atul12345"
+
+
+// "aadhaarNo":634574518213,
+// "firstName":"Mridul",
+// "lastName":"Pandey",
+// "irctcID":"mridul12345",
+// "email":"mridulpandey99@gmail.com",
+// "contactNo":7715859073,
+// "password":"mridul12345"
